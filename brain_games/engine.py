a3604429f6f78048d5d7de9f2cb140e371664909
Artour Eldarov <arthureldarovic@gmail.com>
2024-02-05 19:22:18 +0300

    test

--- brain_games/engine.py
+++ brain_games/engine.py
@@ -4,14 +4,14 @@ from brain_games.consts import AMOUNT_OF_ROUNDS
 
 def run_game(get_question_and_answer: callable, instruction):
     print("Welcome to the Brain Games!")
-    name = prompt.string("May I have your name? ")
+    name = prompt.string(empty=True, prompt="May I have your name? ")
     print(f'Hello, {name}!\n'
           f'{instruction}')
 
     for _ in range(AMOUNT_OF_ROUNDS):
         question, correct_answer = get_question_and_answer()
 
-        user_answer = prompt.string(f'Question: {question}\n'
+        user_answer = prompt.string(empty=True, prompt=f'Question: {question}\n'
                                     f'Your answer: ')
 
         if user_answer == correct_answer:
