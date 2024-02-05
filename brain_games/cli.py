d8cb18bf072f32b56db3de4e96229bcb87db8575
Artour Eldarov <arthureldarovic@gmail.com>
2024-02-05 18:38:09 +0300

    trying to fix bug#1

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -4,8 +4,4 @@ import prompt
 def welcome_user():
     print('Welcome to the Brain Games!')
     name = prompt.string('May i have your name? ')
-    print(f'Hello, {name}')
-
-
-if __name__ == '__main__':
-    welcome_user()
+    print(f'Hello, {name}!')
